098b760c11fe42d0ed5a81163ddd902ca96346ff
Souvik <souvikchandra9454@gmail.com>
2022-06-06 00:36:04 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState}from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from 'expo-location';
 
+import DateTime from './components/DateTime'
+import WeatherScroll from './components/WeatherScroll'
+const API_KEY ='e96f979e3f4ebb6cb47177c90dcefadb';
+const img = require('./assets/image.png')
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        fetchDataFromApi("40.7128", "-74.0060")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if(latitude && longitude) {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+      // console.log(data)
+      setData(data)
+      })
+    }
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={img} style={styles.image} >
+        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+        <WeatherScroll weatherData={data.daily}/>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +47,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image:{
+    flex:1, 
+    resizeMode:"cover", 
+    justifyContent:"center"
+  }
 });
